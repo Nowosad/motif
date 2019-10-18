773e85b1583a2f3a131ef6a6989a7ab65f18abb0
nowosad <dukekubas@gmail.com>
2019-10-18 11:05:56 +0200

    adds lop_cluster lop_segment

---
+++ R/lop_cluster.R
@@ -0,0 +1,31 @@
+# x = lop_grid(lc, size = 100)
+# ids = cutree(coma_hclust, k = 12)
+
+lop_clusters = function(x, ids){
+  x[["clust"]] = ids
+  x[["motifels"]] = seq_len(nrow(x))
+  x_sum = dplyr::summarise(dplyr::group_by(x, clust))
+  x_sum = sf::st_collection_extract(x_sum, "POLYGON")
+
+  df_sum = tidyr::nest(st_drop_geometry(x), data = c(motifels))
+  dplyr::left_join(x_sum, df_sum, by = "clust")
+}
+
+lop_segments = function(x, ids){
+  x[["clust"]] = ids
+  x[["motifels"]] = seq_len(nrow(x))
+  x_sum = dplyr::summarise(dplyr::group_by(x, clust))
+  x_sum = sf::st_collection_extract(x_sum, "POLYGON")
+  x[["clust"]] = NULL
+
+  x_seg = st_cast(x_sum, "POLYGON")
+  x_seg$clust = seq_len(nrow(x_seg))
+
+  x2 = st_join(x, x_seg, largest = TRUE)
+
+  df_sum = tidyr::nest(st_drop_geometry(x2), data = c(motifels))
+  dplyr::left_join(x_seg, df_sum, by = "clust")
+}
+
+x_clu = lop_clusters(x, ids)
+x_seg = lop_segments(x, ids)
