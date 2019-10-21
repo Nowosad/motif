674f175bf6d0e16a7a36e27bf66527e97ce40b76
nowosad <dukekubas@gmail.com>
2019-10-21 14:08:57 +0200

    improves lop_cluster

--- R/lop_cluster.R
+++ R/lop_cluster.R
@@ -1,5 +1,26 @@
+# library(comat)
+# library(raster)
+# library(dplyr)
+# library(sf)
+# landcover = raster(system.file("raster/landcover.tif", package = "lopata"))
+# # plot(landcover)
+# lc_coma = lop_coma(landcover, size = 100)
+# lc_grid = lop_grid(landcover, size = 100) %>%
+#   inner_join(lc_coma[c("row", "col")], by = c("row", "col"))
+# lc_cove = lop_cove(lc_coma, normalization = "pdf", ordered = FALSE)
+# lc_dist = lop_to_dist(lc_cove, method = "jensen-shannon")
+# lc_hclust = hclust(lc_dist, method = "ward.D2")
+# lc_ids = cutree(lc_hclust, k = 12)
+#
+# x = lc_grid
+# ids = lc_ids
+
+
 # x = lop_grid(lc, size = 100)
 # ids = cutree(coma_hclust, k = 12)
+# x_clu = lop_clusters(x, ids)
+# x_seg = lop_segments(x, ids)
+
 #' lop_clusters
 #' @export
 lop_clusters = function(x, ids){
@@ -8,7 +29,7 @@ lop_clusters = function(x, ids){
   x_sum = dplyr::summarise(dplyr::group_by(x, clust))
   x_sum = sf::st_collection_extract(x_sum, "POLYGON")
 
-  df_sum = tidyr::nest(st_drop_geometry(x), data = c(motifels))
+  df_sum = tidyr::nest(st_drop_geometry(x[c("clust", "motifels")]), data = c(motifels))
   dplyr::left_join(x_sum, df_sum, by = "clust")
 }
 #' lop_segments
@@ -25,9 +46,8 @@ lop_segments = function(x, ids){
 
   x2 = st_join(x, x_seg, largest = TRUE)
 
-  df_sum = tidyr::nest(st_drop_geometry(x2), data = c(motifels))
+  df_sum = tidyr::nest(st_drop_geometry(x2[c("clust", "motifels")]), data = c(motifels))
   dplyr::left_join(x_seg, df_sum, by = "clust")
 }
 
-# x_clu = lop_clusters(x, ids)
-# x_seg = lop_segments(x, ids)
+
