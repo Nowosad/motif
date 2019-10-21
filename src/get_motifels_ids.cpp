5020df8c584dff8f1d9a853fc6d2e815bbd32a6e
nowosad <dukekubas@gmail.com>
2019-10-21 14:07:36 +0200

    adds get_motigels_ids

---
+++ src/get_motifels_ids.cpp
@@ -0,0 +1,30 @@
+#include "get_motifels_ids.h"
+
+// [[Rcpp::export]]
+IntegerMatrix get_motifels_ids(int num_r, int num_c, int size, int shift) {
+
+  int nr_of_motifels = 0;
+  for (int i = 0; i < num_r; i = i + shift) {
+    for (int j = 0; j < num_c; j = j + shift) {
+      nr_of_motifels ++;
+    }
+  }
+
+  IntegerMatrix result(nr_of_motifels, 2);
+
+  int m = 0;
+  int m_row = 1;
+  int m_col = 1;
+
+  for (int i = 0; i < num_r; i = i + shift){
+    for (int j = 0; j < num_c; j = j + shift){
+      result(m, 0) = m_row;
+      result(m, 1) = m_col;
+      m++;
+      m_col++;
+    }
+    m_col = 1;
+    m_row++;
+  }
+  return result;
+}
---
+++ src/get_motifels_ids.h
@@ -0,0 +1,8 @@
+#ifndef get_motifels_ids_H
+#define get_motifels_ids_H
+#include "Rcpp.h"
+using namespace Rcpp;
+
+IntegerMatrix get_motifels_ids(int num_r, int num_c, int size, int shift);
+
+#endif // get_motifels_ids_H
