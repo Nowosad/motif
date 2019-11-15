b4b6355b77646baa9331361c8b3a279b70ea39f9
Nowosad <tupiszakaczki@gmail.com>
2019-11-15 10:44:01 +0100

    adds na_prop_arma

--- src/na_prop.cpp
+++ src/na_prop.cpp
@@ -1,7 +1,7 @@
 #include "na_prop.h"
 
 // calculates a proportion of cells with NA's
-double na_prop(IntegerMatrix x) {
+double na_prop(const IntegerMatrix& x) {
 
   double na_of_cells = x.length();
   double na_of_na = std::count_if(x.begin(), x.end(),
@@ -9,6 +9,17 @@ double na_prop(IntegerMatrix x) {
 
   return na_of_na / na_of_cells;
 }
+
+// calculates a proportion of cells with NA's
+double na_prop_arma(const arma::imat& x) {
+
+  double na_of_cells = x.size();
+  double na_of_na = std::count_if(x.begin(), x.end(),
+                                  [](double x){return x == NA_INTEGER;});
+
+  return na_of_na / na_of_cells;
+}
+
 
 /*** R
 a = matrix(c(1:9), ncol = 3)
@@ -18,4 +29,5 @@ d = matrix(c(1:4, rep(NA, 5)), ncol = 3)
 na_prop(a)
 na_prop(b)
 na_prop(d)
+na_prop2(d)
 */
--- src/na_prop.h
+++ src/na_prop.h
@@ -1,11 +1,14 @@
 #ifndef na_prop_H
 #define na_prop_H
-#include "Rcpp.h"
+#include "RcppArmadillo.h"
 #include <algorithm>     // for count_if
 // [[Rcpp::plugins(cpp11)]]
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-double na_prop(Rcpp::IntegerMatrix x);
+double na_prop(const IntegerMatrix& x);
+
+// [[Rcpp::export]]
+double na_prop_arma(const arma::imat& x);
 
 #endif // na_prop_H
