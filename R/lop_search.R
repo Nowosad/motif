ca9f6133096862d3160f462717eac7e295213c3c
Nowosad <tupiszakaczki@gmail.com>
2019-11-29 13:52:07 +0100

    fixes fun style

--- R/lop_search.R
+++ R/lop_search.R
@@ -29,32 +29,67 @@ lop_search = function(x, y, type, dist_fun, window, window_size = NULL, window_s
                       neighbourhood = 4, threshold = 0.5, ordered = TRUE, repeated = TRUE,
                       normalization = "pdf", wecoma_fun = "mean", wecoma_na_action = "replace"){
 
-  output = lop_thumbprint(y, type = type, neighbourhood = neighbourhood, window, window_size = window_size, window_shift = window_shift,
-                          threshold = threshold, ordered = ordered, repeated = repeated,
-                          normalization = normalization, wecoma_fun = wecoma_fun, wecoma_na_action = wecoma_na_action)
+  output = lop_thumbprint(
+    y,
+    type = type,
+    neighbourhood = neighbourhood,
+    window,
+    window_size = window_size,
+    window_shift = window_shift,
+    threshold = threshold,
+    ordered = ordered,
+    repeated = repeated,
+    normalization = normalization,
+    wecoma_fun = wecoma_fun,
+    wecoma_na_action = wecoma_na_action
+  )
 
   unique_classes_all = attributes(output)[["metadata"]][["vals"]]
 
   x = lapply(x, raster::as.matrix)
 
-  if (type == "coma" || type == "cove"){
-    input_thumbprint = comat::get_cove(comat::get_coma(x[[1]], neighbourhood = neighbourhood, classes = unique_classes_all),
-                                       ordered = ordered, normalization = normalization)
-  } else if (type == "cocoma" || type == "cocove"){
-    input_thumbprint = comat::get_cocove(comat::get_cocoma(x[[1]], x[[2]], neighbourhood = neighbourhood, classes = unique_classes_all),
-                                         ordered = ordered, normalization = normalization)
-  } else if (type == "wecoma" || type == "wecove"){
-    input_thumbprint = comat::get_wecove(comat::get_wecoma(x[[1]], x[[2]], neighbourhood = neighbourhood, classes = unique_classes_all,
-                                                           fun = wecoma_fun, na_action = wecoma_na_action),
-                                         ordered = ordered, normalization = normalization)
-  } else if (type == "incoma" || type == "incove"){
-    input_thumbprint = comat::get_incove(comat::get_incoma(x, neighbourhood = neighbourhood, classes = unique_classes_all),
-                                         ordered = ordered, repeated = repeated, normalization = normalization)
+  if (type == "coma" || type == "cove") {
+    input_thumbprint = comat::get_cove(
+      comat::get_coma(x[[1]], neighbourhood = neighbourhood, classes = unique_classes_all),
+      ordered = ordered,
+      normalization = normalization
+    )
+  } else if (type == "cocoma" || type == "cocove") {
+    input_thumbprint = comat::get_cocove(
+      comat::get_cocoma(x[[1]], x[[2]], neighbourhood = neighbourhood, classes = unique_classes_all),
+      ordered = ordered,
+      normalization = normalization
+    )
+  } else if (type == "wecoma" || type == "wecove") {
+    input_thumbprint = comat::get_wecove(
+      comat::get_wecoma(
+        x[[1]],
+        x[[2]],
+        neighbourhood = neighbourhood,
+        classes = unique_classes_all,
+        fun = wecoma_fun,
+        na_action = wecoma_na_action
+      ),
+      ordered = ordered,
+      normalization = normalization
+    )
+  } else if (type == "incoma" || type == "incove") {
+    input_thumbprint = comat::get_incove(
+      comat::get_incoma(x, neighbourhood = neighbourhood, classes = unique_classes_all),
+      ordered = ordered,
+      repeated = repeated,
+      normalization = normalization
+    )
   }
 
-  output$dist = unlist(lapply(output$signature, dist_fun,
-                              P = input_thumbprint, testNA = FALSE,
-                              unit = "log2"))
+  output$dist = unlist(lapply(
+    output$signature,
+    dist_fun,
+    P = input_thumbprint,
+    testNA = FALSE,
+    unit = "log2"
+  ))
+
   output$signature = NULL
   return(output)
 }
