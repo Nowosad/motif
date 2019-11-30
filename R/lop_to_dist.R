fe63bccf6f418a4aa71362deb305ae4fa6492bec
Nowosad <tupiszakaczki@gmail.com>
2019-11-30 10:06:48 +0100

    updates lop_to_dist

--- R/lop_to_dist.R
+++ R/lop_to_dist.R
@@ -3,7 +3,7 @@
 #' Calculates a distance matrix based on an object of class `cove`, `cocove`, `wecove`, or `incove`
 #'
 #' @param x An object of class `cove`, `cocove`, `wecove`, or `incove`
-#' @param method A distance/dissimilarity method used. All possible values can be found using
+#' @param dist_fun A distance/dissimilarity method used. All possible values can be found using
 #' the [philentropy::getDistMethods()] function
 #' @param unit A character string specifying the logarithm unit that should be used to
 #' compute distances that depend on log computations: `"log", "log2", "log10"`.
@@ -17,45 +17,22 @@
 #'
 #' @examples
 #' library(comat)
-#' library(raster)
+#' library(stars)
 #' data(raster_x, package = "comat")
-#' raster_x = raster(raster_x)
+#' raster_x = st_as_stars(raster_x)
 #' plot(raster_x)
 #'
-#' com = lop_coma(raster_x, size = 2)
-#' com
+#' cov = lop_thumbprint(raster_x, window_size = 2, type = "cove")
 #'
-#' cov = lop_cove(com)
-#' cov
-#'
-#' dist_cov = lop_to_dist(cov, method = "jensen-shannon")
+#' dist_cov = lop_to_dist(cov, dist_fun = "jensen-shannon")
 #' dist_cov
 #' @export
-lop_to_dist = function(x, method, unit, p) UseMethod("lop_to_dist")
-#'
-#' @name lop_to_dist
-#' @export
-lop_to_dist.cove = function(x, method, unit = "log", p = NULL){
-  vec_to_dist(vec = x$vector, method = method, unit = unit, p = p)
-}
-#' @name lop_to_dist
-#' @export
-lop_to_dist.cocove = function(x, method, unit = "log", p = NULL){
-  vec_to_dist(vec = x$vector, method = method, unit = unit, p = p)
-}
-#' @name lop_to_dist
-#' @export
-lop_to_dist.wecove = function(x, method, unit = "log", p = NULL){
-  vec_to_dist(vec = x$vector, method = method, unit = unit, p = p)
-}
-#' @name lop_to_dist
-#' @export
-lop_to_dist.incoma = function(x, method, unit = "log", p = NULL){
-  vec_to_dist(vec = x$vector, method = method, unit = unit, p = p)
+lop_to_dist = function(x, dist_fun, unit = "log2", p = NULL){
+  vec_to_dist(vec = x$signature, dist_fun = dist_fun, unit = unit, p = p)
 }
 
-vec_to_dist = function(vec, method, unit, p){
+vec_to_dist = function(vec, dist_fun, unit, p){
   mat = do.call(rbind, vec)
-  mat = philentropy::distance(mat, method = method, p = p, test.na = FALSE, unit = unit)
+  mat = philentropy::distance(mat, method = dist_fun, p = p, test.na = FALSE, unit = unit)
   stats::as.dist(mat)
 }
