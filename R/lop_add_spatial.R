7e70387f86719e013f9e94366f2ffa4eec85aaa6
Nowosad <tupiszakaczki@gmail.com>
2019-12-09 17:46:01 +0100

    adds lsp method

--- R/lop_add_spatial.R
+++ R/lop_add_spatial.R
@@ -2,12 +2,18 @@
 #' library(stars)
 #' landcover = read_stars(system.file("raster/landcover2015.tif", package = "lopata"))
 #' plot(landcover)
-#' x = landcover
 #' lop_add_spatial(landcover, window_size = 100)
 #'
+#' lc_cove = lop_thumbprint(landcover, type = "cove", window_size = 200, normalization = "pdf")
+#' lop_add_spatial(lc_cove)
 #'
 #' # ?fill_na
 #' landform = read_stars(system.file("raster/landform.tif", package = "lopata"))
+#'
+#' lf_cove = lop_thumbprint(landform, type = "cove", window_size = 200, normalization = "pdf")
+#' lop_add_spatial(lf_cove)
+#'
+#'
 #' my_fun = function(x) landscapemetrics::lsm_l_ent(x, neighbourhood = 4, base = "log2")[["value"]]
 #' ee = lop_thumbprint(landform, type = my_fun, threshold = 0.2, window_size = 100)
 #'
@@ -19,44 +25,33 @@
 #'
 #' plot(output["values2"])
 #'
-lop_add_spatial = function(x = NULL, window = NULL, window_size = NULL, window_shift = NULL){
+#' @aliases lop_add_spatial
+#' @rdname lop_add_spatial
+#'
+#' @export
+lop_add_spatial = function(x = NULL, window = NULL, window_size = NULL, window_shift = NULL) UseMethod("lop_add_spatial")
+
+#' @name lop_add_spatial
+#' @export
+lop_add_spatial.default = function(x = NULL, window = NULL, window_size = NULL, window_shift = NULL){
 
   if (!missing(window_size) && !is.null(window_size)){
     x_crs = st_crs(x)
     x_nrow = nrow(x)
     x_ncol = ncol(x)
 
-    if (is.null(window_shift)){
-      window_shift = window_size
-    }
-
     x_bb = sf::st_bbox(x)
 
     x_delta_row = st_dimensions(x)[[1]][["delta"]]
     x_delta_col = st_dimensions(x)[[2]][["delta"]]
 
-    cellshift = c(window_shift * x_delta_row,
-                  window_shift * x_delta_col)
+    if (is.null(window_shift)){
+      window_shift = window_size
+    }
 
-    output_n_row = ceiling(abs((x_bb["xmax"] - x_bb["xmin"]) / cellshift[1]))
-    output_n_col = ceiling(abs((x_bb["ymin"] - x_bb["ymax"]) / cellshift[1]))
-
-    new_xmax = x_bb["xmin"] + (output_n_row * cellshift[1])
-    new_ymin = x_bb["ymax"] + (output_n_col * cellshift[2])
-
-    output_bb = st_bbox(c(
-      xmin = unname(x_bb["xmin"]),
-      ymin = unname(new_ymin),
-      xmax = unname(new_xmax),
-      ymax = unname(x_bb["ymax"])
-    ))
-
-    output = st_as_stars(output_bb,
-                         nx = output_n_row,
-                         ny = output_n_col,
-                         values = as.integer(seq_len(output_n_row * output_n_col)))
-    output = st_set_crs(output, value = x_crs)
-    names(output) = "id"
+    output = lop_create_grid(x_crs = x_crs, x_bb = x_bb,
+                             x_delta_row = x_delta_row, x_delta_col = x_delta_col,
+                             window_shift = window_shift)
 
     # df_ids = get_motifels_ids(x_nrow, x_ncol, size, shift)
     # output$col = df_ids[, 1]
@@ -76,4 +71,61 @@ lop_add_spatial = function(x = NULL, window = NULL, window_size = NULL, window_s
     names(window) = "id"
     return(window)
   }
+}
+
+#' @name lop_add_spatial
+#' @export
+lop_add_spatial.lsp = function(x = NULL, window = NULL, window_size = NULL, window_shift = NULL){
+  metadata = attr(x, "metadata")
+
+  output_stars = lop_create_grid(x_crs = metadata$crs, x_bb = metadata$bb,
+                           x_delta_row = metadata$delta_y, x_delta_col = metadata$delta_x,
+                           window_shift = metadata$window_shift)
+
+  true_dim = dim(output_stars$id)
+  matched_ids = match(output_stars$id, x$id)
+  # matched_ids = matched_ids[-1]
+
+  for (i in 2:ncol(x)){
+    selected_colnames = colnames(x)[i]
+    selected_vals = x[[selected_colnames]][matched_ids]
+    if (inherits(selected_vals, "list")){
+      warning("Column ", selected_colnames, " is of a list class and will be dropped from the output object.",
+      call. = FALSE)
+    } else {
+      output_stars[[selected_colnames]] = selected_vals
+      dim(output_stars[[selected_colnames]]) = true_dim
+    }
+  }
+  return(output_stars)
+}
+
+
+lop_create_grid = function(x_crs, x_bb, x_delta_row, x_delta_col, window_shift){
+
+
+  cellshift = c(window_shift * x_delta_row,
+                window_shift * x_delta_col)
+
+  output_n_row = ceiling(abs((x_bb["xmax"] - x_bb["xmin"]) / cellshift[1]))
+  output_n_col = ceiling(abs((x_bb["ymin"] - x_bb["ymax"]) / cellshift[1]))
+
+  new_xmax = x_bb["xmin"] + (output_n_row * cellshift[1])
+  new_ymin = x_bb["ymax"] + (output_n_col * cellshift[2])
+
+  output_bb = st_bbox(c(
+    xmin = unname(x_bb["xmin"]),
+    ymin = unname(new_ymin),
+    xmax = unname(new_xmax),
+    ymax = unname(x_bb["ymax"])
+  ))
+
+  output = st_as_stars(output_bb,
+                       nx = output_n_row,
+                       ny = output_n_col,
+                       values = as.integer(seq_len(output_n_row * output_n_col)))
+  output = st_set_crs(output, value = x_crs)
+  names(output) = "id"
+
+  return(output)
 }
