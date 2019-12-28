2bc3079b99c87cde8e2a2d38bdd9dc2ddd942098
Nowosad <tupiszakaczki@gmail.com>
2019-12-28 16:26:40 +0100

    adds lsp_add_spatial tests

---
+++ tests/testthat/test-lsp_add_spatial.R
@@ -0,0 +1,27 @@
+library(stars)
+landform = read_stars(system.file("raster/landform.tif", package = "motif"))
+plot(landform)
+
+lc_cove = lsp_thumbprint(landform, type = "cove", window_size = 100, normalization = "pdf")
+lc_cove
+
+# stars1 ------------------------------------------------------------------
+landform_lsp_stars = lsp_add_stars(landform, window_size = 100)
+plot(landform_lsp_stars)
+
+# stars2 ------------------------------------------------------------------
+lc_cove_lsp_stars = lsp_add_stars(lc_cove)
+
+plot(lc_cove_lsp_stars)
+plot(lc_cove_lsp_stars["na_prop"])
+
+# sf1 ---------------------------------------------------------------------
+landform_lsp_sf = lsp_add_sf(landform, window_size = 100)
+plot(landform_lsp_sf)
+
+# sf2 ---------------------------------------------------------------------
+lc_cove_lsp_sf = lsp_add_sf(lc_cove)
+
+plot(lc_cove_lsp_sf["id"])
+plot(lc_cove_lsp_sf["na_prop"])
+
