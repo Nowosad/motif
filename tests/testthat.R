ddb84e61101c1d0eaa4efe9cd20062f3a8e03574
Nowosad <tupiszakaczki@gmail.com>
2020-03-14 19:29:07 +0100

    cleans tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -9,6 +9,7 @@ landform_p = read_stars(system.file("raster/landform.tif", package = "motif"), p
 landcover_p = read_stars(system.file("raster/landcover2015.tif", package = "motif"), proxy = TRUE)
 ecoregions = read_sf(system.file("vector/ecoregions.gpkg", package = "motif"))
 set.seed(222)
+# st_crs(ecoregions) = st_crs(landcover)
 random_ndvi = landcover
 random_ndvi$ndvi = sample(x = 1:10, size = length(random_ndvi[[1]]), replace = TRUE)
 random_ndvi$landcover2015.tif = NULL
