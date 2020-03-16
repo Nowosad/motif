5400fbfe71d5114998aaf7036272c4525ac32a98
Nowosad <tupiszakaczki@gmail.com>
2020-03-16 11:30:31 +0100

    complates search tests

--- tests/testthat/test-lsp_search.R
+++ tests/testthat/test-lsp_search.R
@@ -32,6 +32,33 @@ s_incove = lsp_search(
   threshold = 0.9
 )
 
+s_cove_motiel = lsp_search(
+  landform_ext,
+  landform,
+  type = "cove",
+  dist_fun = "jensen-shannon",
+  threshold = 0.5,
+  window_size = 250
+)
+
+s_cove_proxy = lsp_search(
+  landform_ext,
+  landform_p,
+  type = "cove",
+  dist_fun = "jensen-shannon",
+  threshold = 0.5,
+  window_size = 250
+)
+
+s_cove_window = lsp_search(
+  landform_ext,
+  landform,
+  type = "cove",
+  dist_fun = "jensen-shannon",
+  threshold = 1,
+  window = ecoregions["id"]
+)
+
 test_that("tests simple search results", {
   expect_equivalent(unlist(unique(s_cove)),
                     c(1, 0.6806109, 0.4454181),
@@ -47,41 +74,18 @@ test_that("tests simple search results", {
                     tolerance = .001)
 })
 
-#
-# # all area ----------------------------------------------------------------
-# s1 = lsp_search(lc_ext, lc, type = "cove",
-#   dist_fun = "jensen-shannon", threshold = 0.9)
-#
-# # cocove ------------------------------------------------------------------
-# s2 = lsp_search(c(lc_ext, lf_ext), c(lc, lf), type = "cocove",
-#   dist_fun = "jensen-shannon", threshold = 0.9)
-#
-# # wecove ------------------------------------------------------------------
-# s3 = lsp_search(c(lc_ext, rn_ext), c(lc, rn), type = "wecove",
-#   dist_fun = "jensen-shannon", threshold = 0.9)
-#
-# # incove ------------------------------------------------------------------
-# s4 = lsp_search(c(lc_ext, lf_ext), c(lc, lf), type = "incove",
-#   dist_fun = "jensen-shannon", threshold = 0.9)
-#
-# # motifel -----------------------------------------------------------------
-# s5 = lsp_search(lf_ext, lf, type = "cove",
-#   dist_fun = "jensen-shannon", threshold = 0.5, window_size = 250)
-#
-# # window ------------------------------------------------------------------
-# s6 = lsp_search(lc_ext, lc, type = "cove",
-#   dist_fun = "jensen-shannon", threshold = 1, window = ecoregions["id"])
-#
-# # motifel proxy -----------------------------------------------------------
-# s7 = lsp_search(lf_ext, lfp, type = "cove",
-#                 dist_fun = "jensen-shannon", threshold = 0.5, window_size = 250)
-#
-# # window proxy ------------------------------------------------------------
-# s8 = lsp_search(lc_ext, lcp, type = "cove",
-#                 dist_fun = "jensen-shannon", threshold = 1, window = ecoregions["id"])
-#
-# # all.equal(s6, s8)
-# # bench::mark(lsp_search(lf_ext, lf, type = "cove",
-# #                        dist_fun = "jensen-shannon", threshold = 0.5, window_size = 250),
-# #             lsp_search(lf_ext, lfp, type = "cove",
-# #                        dist_fun = "jensen-shannon", threshold = 0.5, window_size = 250))
+test_that("tests motifel search results", {
+  expect_equal(sum(is.na(s_cove_motiel$dist)), 341)
+  expect_equivalent(min(s_cove_motiel$dist, na.rm = TRUE), 0.0001, tolerance = .001)
+  expect_equivalent(mean(s_cove_motiel$dist, na.rm = TRUE), 0.5727, tolerance = .001)
+  expect_equivalent(max(s_cove_motiel$dist, na.rm = TRUE), 1, tolerance = .001)
+})
+
+test_that("tests proxy search results", {
+  expect_equal(s_cove_proxy, s_cove_motiel)
+})
+
+test_that("tests window search results", {
+  expect_equal(sort(unique(c(s_cove_window$id))), 1:22)
+  expect_equivalent(min(s_cove_window$dist, na.rm = TRUE), 0.00029, tolerance = .001)
+})
