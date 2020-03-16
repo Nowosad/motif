4dd936d7c2684144a773e0e68ed8d2e4ce75df7f
Nowosad <tupiszakaczki@gmail.com>
2020-03-16 10:02:02 +0100

    adds sample data prep

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,15 +3,31 @@ library(stars)
 library(sf)
 library(motif)
 
+# prepare regular stars ---------------------------------------------------
 landcover = read_stars(system.file("raster/landcover2015.tif", package = "motif"))
 landform = read_stars(system.file("raster/landform.tif", package = "motif"))
+
+# prepare stars proxy -----------------------------------------------------
 landform_p = read_stars(system.file("raster/landform.tif", package = "motif"), proxy = TRUE)
 landcover_p = read_stars(system.file("raster/landcover2015.tif", package = "motif"), proxy = TRUE)
+
+# prepare poly ------------------------------------------------------------
 ecoregions = read_sf(system.file("vector/ecoregions.gpkg", package = "motif"))
 set.seed(222)
 # st_crs(ecoregions) = st_crs(landcover)
+
+# prepare cont data -------------------------------------------------------
 random_ndvi = landcover
 random_ndvi$ndvi = sample(x = 1:10, size = length(random_ndvi[[1]]), replace = TRUE)
 random_ndvi$landcover2015.tif = NULL
+
+# prepare sample landscapes -----------------------------------------------
+ext = st_bbox(c(xmin = -249797.344531127, xmax = -211162.693944285,
+                ymin = -597280.143035389, ymax = -558645.492448547),
+                crs = st_crs(lc))
+
+landcover_ext = landcover[ext]
+landform_ext = landform[ext]
+random_ndvi_ext = random_ndvi[ext]
 
 test_check("motif")
