fad6de5329a2f45e0100ad71bba6620df1148219
Nowosad <tupiszakaczki@gmail.com>
2020-03-17 13:13:31 +0100

    cleans old code

--- tests/testthat/test-lsp_compare.R
+++ tests/testthat/test-lsp_compare.R
@@ -62,39 +62,3 @@ test_that("tests compare cocove results", {
 test_that("tests compare wecove results", {
   expect_equal(c_wecove$dist[[1]], 0.0002, tolerance = .001)
 })
-
-# # all area ----------------------------------------------------------------
-# s1 = lsp_compare(lc01, lc15, type = "cove",
-#   dist_fun = "jensen-shannon", threshold = 0.9)
-#
-# # window ------------------------------------------------------------------
-# s1b = lsp_compare(lc01, lc15, type = "cove",
-#   dist_fun = "jensen-shannon", window = ecoregions["id"], threshold = 0.9)
-#
-# # motifel -----------------------------------------------------------------
-# s1c = lsp_compare(lc01, lc15, type = "cove",
-#   dist_fun = "jensen-shannon", window_size = 100, threshold = 0.9)
-#
-# # cocove ------------------------------------------------------------------
-# s2 = lsp_compare(c(lc01, lf), c(lc15, lf), type = "cocove",
-#   dist_fun = "jensen-shannon", threshold = 0.9)
-#
-# # wecove ------------------------------------------------------------------
-# s3 = lsp_compare(c(lc01, lf), c(lc15, lf), type = "wecove",
-#   dist_fun = "jensen-shannon", threshold = 0.9)
-#
-# # incove ------------------------------------------------------------------
-# s4 = lsp_compare(c(lc01, lf), c(lc15, lf), type = "incove",
-#   dist_fun = "jensen-shannon", threshold = 0.9)
-#
-# # proxy motifel -----------------------------------------------------------
-# s5 = lsp_compare(lc01p, lc15p, type = "cove",
-#                   dist_fun = "jensen-shannon", window_size = 100, threshold = 0.9)
-#
-# # all.equal(s1c$dist, s5$dist)
-#
-# # proxy window ------------------------------------------------------------
-# s6 = lsp_compare(lc01p, lc15p, type = "cove",
-#                   dist_fun = "jensen-shannon", window = ecoregions["id"], threshold = 0.9)
-#
-# # all.equal(s1b$dist, s6$dist)
