5388956d28528eebd366f2c3cbf760fcf82c23f9
Nowosad <tupiszakaczki@gmail.com>
2020-03-17 12:02:46 +0100

    adds new add spatial tests

--- tests/testthat/test-lsp_add_spatial.R
+++ tests/testthat/test-lsp_add_spatial.R
@@ -1,7 +1,11 @@
 context("add spatial")
 
 landform_lsp_sf = lsp_add_sf(landform, window_size = 100)
+landform_lsp_sf2 = lsp_add_sf(landform)
+landform_lsp_sf3 = lsp_add_sf(landform, window = ecoregions["id"])
 
 test_that("tests landform_lsp_sf works on default", {
   expect_equal(dim(landform_lsp_sf), c(2886, 2))
+  expect_equal(dim(landform_lsp_sf2), c(1, 2))
+  expect_equal(dim(landform_lsp_sf3), c(22, 2))
 })
